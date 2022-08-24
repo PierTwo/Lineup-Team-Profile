774cdbd8f73bc32c8035dff87b73b20b39b476e4
PierTwo <105090994+PierTwo@users.noreply.github.com>
2022-08-24 01:27:04 -0500

    Added comments to writeToFile.js

--- src/writeToFile.js
+++ src/writeToFile.js
@@ -1,26 +1,18 @@
+// Imports required module to create the HTML file
 const fs = require("fs");
 
+// Creates the HTML file with the HTML data that was passed to the function
 function writeToFile(htmlData) {
-  // fs.readFile("./dist/index.html", "utf8", (err, data) => {
-  //   if (err) {
-  //     return console.error(err);
-  //   } else {
-  //     const htmlResult = data.replace(
-  //       /\<div class="d-none"><\/div>/g,
-  //       htmlData + `<div class="d-none"></div>`
-  //     );
-  //     fs.writeFile("./dist/index.html", htmlResult, "utf8", (err) => {
-  //       return err
-  //         ? console.error(err)
-  //         : console.log("Succesfully added team member!");
-  //     });
-  //   }
-  // });
+  // Writes file to the /dist directory with the HTML data passed to the function using UTF-8 character encoding
   fs.writeFile("./dist/index.html", htmlData, "utf8", (err) => {
+    // If the err callback is truthy then write the error to the console. Otherwise log that the file was sucessfully created
     return err
       ? console.error(err)
-      : console.log("Succesfully created your team lineup!");
+      : console.log(
+          "Succesfully created your team lineup! to /dist/index.html"
+        );
   });
 }
 
+// Modularize and export the writeToFile function
 module.exports = writeToFile;
